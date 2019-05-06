7a6a4a8a1e7e913e211032a4723dfa85ad517f60
AihamAl <Aiham.Alsalehy@gmail.com>
2019-05-06 14:09:28 +0200

    Add files via upload

---
+++ Hiss.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+class Elevated {	
+    private static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	int i = sc.nextInt();
+    		
+    	if (i == 1) {
+    		System.out.println("1");
+    	}
+    	
+    	if (i == 2) {
+    		System.out.println("2");
+    	}
+    	
+    	if (i == 3) {
+    		System.out.println("3");
+    	}
+	
+	    if (i == 4) {
+		    System.out.println("4");
+	    }
+	
+	    if (i == 5) {
+		    System.out.println("5");
+	    }
+	
+	    if (i == 6) {
+		    System.out.println("6");
+	    }
+	
+	    if (i == 7) {
+		    System.out.println("7");
+	    }
+	
+	    if (i == 8) {
+		    System.out.println("8");
+	    }
+	
+	    if (i == 9) {
+	    	System.out.println("9");
+	    }
+    }
+}
